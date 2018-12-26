95c0cd0d2c5e920aa7556151f0d2e9362bc0392a
SteinmannTh <steinmann.thms@gmail.com>
2018-12-26 16:24:36 +0300

    Started to implement Messenger and Friends. (Social part)

--- src/app/messages-component/messages-component.component.ts
+++ src/app/messages-component/messages-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Message} from '../message';
+import {User} from '../user';
 
 @Component({
   selector: 'app-messages-component',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  inMessages: Message[];
+  outMessages: Message[];
+  
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get<User>('http://localhost:8080/profile', {withCredentials: true})
+      .subscribe(data => this.user = data);
+    
   }
 
 }
