9ba3e92810e3d932c73f062e32a8a34434813ecd
SteinmannTh <steinmann.thms@gmail.com>
2019-01-11 19:05:34 +0300

    websockets for queue component

--- src/app/queue/queue.component.ts
+++ src/app/queue/queue.component.ts
@@ -1,12 +1,14 @@
 import {Component, Injector, OnInit} from '@angular/core';
 import {AreaService} from '../services/area/area.service';
-import {HttpClient, HttpSentEvent} from '@angular/common/http';
+import {HttpClient, HttpParams, HttpSentEvent} from '@angular/common/http';
 import {CookieService} from 'ngx-cookie-service';
 import {Button} from 'primeng/button';
 import {FightComponent} from '../fight/fight.component';
 import {FightService} from '../services/fight/fight.service';
 import {User} from '../classes/user';
 import {MainComponent} from '../main/main.component';
+import {Stomp} from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-queue',
@@ -18,6 +20,7 @@ export class QueueComponent implements OnInit {
   area: string;
   users: string[];
   selected: string[];
+  private stompClient;
   type: string;
   disabled: boolean;
   parent = this.injector.get(MainComponent);
@@ -65,7 +68,9 @@ export class QueueComponent implements OnInit {
           withCredentials: true
         }).subscribe(enemy => {
           this.fightService.enemies = [enemy];
+          this.send('pvp', this.selected[0]);
           this.parent.router.navigateByUrl('fight');
+          
           // TODO close dialog
         });
       });
@@ -73,6 +78,7 @@ export class QueueComponent implements OnInit {
       this.fightService.allies = [];
       this.selected.push(this.parent.login);
       for (let i = 0; i < this.selected.length; i++) {
+        this.send('pve', this.selected[i]);
         this.http.get<User>('http://localhost:31480/users/' + this.selected[i], {
           withCredentials: true
         }).subscribe(ally => {
@@ -84,4 +90,25 @@ export class QueueComponent implements OnInit {
       }
     }
   }
+
+  send(type: string, username: string): void {
+    this.http.get('http://localhost:31480/sendinvite', {withCredentials: true,
+  params: new HttpParams().append('type', type).append('username', username)})
+    .subscribe();
+  }
+
+  initializeWebsockets (): void {
+    let ws = new SockJS("http://localhost:31480/socket");
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/user/invite", (response) => {
+        var message: string = response.body; // format: {pvp/pve}:{sender-name}
+        var type: string = message.substring(0, 3);
+        var author: string = message.substring(4, message.length);
+        // do sth
+      });
+    });
+  }
+
 }
