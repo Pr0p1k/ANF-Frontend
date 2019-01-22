03731ef68fd6745091f7a4df2b17f0806ead58af
SteinmannTh <steinmann.thms@gmail.com>
2019-01-23 00:19:30 +0300

    attack ws is reworked

---
+++ src/app/classes/ninja-animal.ts
@@ -0,0 +1,11 @@
+export class NinjaAnimal {
+
+    name: string;
+    damage: number;
+    maxHP: number;
+    race: string;
+    requiredLevel: number;
+    currentHP: number;
+    currentChakra: number;
+
+}
